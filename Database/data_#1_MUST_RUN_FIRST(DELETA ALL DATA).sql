7e51944c031a185625252e74e39398e200294962
VuBinh <binh161905@gmail.com>
2021-12-14 08:57:52 +0700

    must_run_first: before set data

---
+++ Database/data_#1_MUST_RUN_FIRST(DELETA ALL DATA).sql
@@ -0,0 +1,19 @@
+﻿USE [HOAYEUTHUONG]
+GO
+
+-- == Xóa tất cả dữ liệu trong bảng ==
+-- disable all triggers
+EXEC sp_MSForEachTable 'DISABLE TRIGGER ALL ON ?'
+
+-- disable all constraints
+EXEC sp_MSForEachTable 'ALTER TABLE ? NOCHECK CONSTRAINT ALL'
+
+-- delete data in all tables
+EXEC sp_MSforeachtable 'SET QUOTED_IDENTIFIER ON; DELETE FROM ?'
+
+-- enable all constraints
+EXEC sp_MSForEachTable 'ALTER TABLE ? WITH CHECK CHECK CONSTRAINT ALL'
+
+-- enable all triggers
+EXEC sp_MSForEachTable 'ENABLE TRIGGER ALL ON ?'
+GO
--- Database/data_SPNL.sql
+++ Database/data_SPNL.sql
@@ -1,17 +1,7 @@
 ﻿USE [HOAYEUTHUONG]
 GO
 
--- == Xóa tất cả dữ liệu trong bảng ==
--- disable all constraints
-EXEC sp_MSForEachTable 'ALTER TABLE ? NOCHECK CONSTRAINT all'
-
--- delete data in all tables
-EXEC sp_MSforeachtable 'DELETE FROM ?'
-
--- enable all constraints
-exec sp_MSForEachTable 'ALTER TABLE ? WITH CHECK CHECK CONSTRAINT all'
-GO
-
+--reset IDENTITY VALUE
 DBCC CHECKIDENT([DS_PHANLOAI], RESEED, 0)
 GO
 
