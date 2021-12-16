2c8616dfae35cdf1773e67c57390e28b8471ad60
tvqphuoc01 <tvqphuoc.01@gmail.com>
2021-12-17 05:49:39 +0700

    add ui

--- ui_code/index.js
+++ ui_code/index.js
@@ -8,8 +8,44 @@ app.use(express.static('public/image'));
 app.set('views', './views');
 app.set('view engine', 'pug');
 
+const sql = require('mssql');
+
+// config for your database
+const config = {
+  user: 'phuoctran',
+  password: '123456789',
+  server: 'localhost',
+  database: 'HOAYEUTHUONG',
+  trustServerCertificate: true,
+};
+
+// connect to your database
+sql.connect(config, function(err) {
+  if (err) console.log(err);
+
+  // create Request object
+  const request = new sql.Request();
+
+  // query to the database and get the records
+  request.query('select * from SANPHAM', function(err, recordset) {
+    if (err) console.log(err);
+
+    // send records as a response
+    console.log(recordset);
+  });
+});
+
 app.get('/', (req, res) => {
   res.render('index');
+});
+
+app.get('/cart', (req, res) => {
+  res.render('cart');
+});
+
+
+app.get('/admin', (req, res) => {
+  res.render('admin');
 });
 
 app.listen(port, () => {
