d987e69a8798f2321b6eb58ff7b0623a5c24098d
THƯƠNG NGUYỄN HOÀI <19127287@student.hcmus.edu.vn>
2021-12-16 15:05:44 +0700

    query and index

---
+++ Query/Index_Partition.sql
@@ -0,0 +1,92 @@
+﻿USE HOAYEUTHUONG
+GO 
+
+------------------------- TỐI ƯU HÓA CÂU TRUY VẤN 1 -------------------------
+
+-- Tạo chỉ mục trên thuộc tính MaKHGui của bảng HOADON bao gồm thêm thuộc thích TongTien
+CREATE NONCLUSTERED INDEX IDX_TV1 ON HOADON(MaKHGui) INCLUDE (TongTien)
+
+-- Thực hiện lại
+SELECT MaKHGui, SUM(TONGTIEN) AS 'Tổng đã chi'
+FROM HOADON HD 
+GROUP BY MaKHGui
+ORDER BY SUM(TONGTIEN) DESC
+
+------------------------- TỐI ƯU HÓA CÂU TRUY VẤN 2 -------------------------
+
+-- Tạo chỉ mục trên thuộc tính ChuDe của bảng CT_HOADON bao gồm thêm thuộc thích TenSP
+CREATE NONCLUSTERED INDEX IDX_TV2
+ON [dbo].[SANPHAM_CHUDE] ([ChuDe])
+INCLUDE ([TenSP])
+
+-- Thực hiện lại
+DECLARE @ChuDe VARCHAR(50)= 'Cây thông'
+
+SELECT MaSP, TenSP
+FROM SANPHAM_CHUDE
+WHERE ChuDe LIKE @ChuDe 
+
+------------------------- TỐI ƯU HÓA CÂU TRUY VẤN 3 -------------------------
+
+-- Tạo chỉ mục trên thuộc tính ThanhTien của bảng CT_HOADON
+CREATE NONCLUSTERED INDEX IDX_TV3 ON CT_HOADON(ThanhTien)
+
+-- Thực hiện lại
+DECLARE @N INT = 100
+
+SELECT TOP(@N) SP.MaSP, SP.TenSP, SUM(CTHD.ThanhTien) AS 'Doanh số'
+FROM CT_HOADON CTHD JOIN SANPHAM SP ON CTHD.MaSP = SP.MaSP
+GROUP BY SP.MaSP, SP.TenSP
+ORDER BY SUM(CTHD.ThanhTien)
+
+------------------------- TỐI ƯU HÓA CÂU TRUY VẤN 4 -------------------------
+
+------------------------- TỐI ƯU HÓA CÂU TRUY VẤN 5 -------------------------
+
+-- Tạo định nghĩa phân đoạn theo từng quý
+CREATE PARTITION FUNCTION PartFunc(DATETIME) 
+AS RANGE RIGHT FOR VALUES ('2021-01-01', '2021-04-01', '2021-07-01', '2021-10-01')
+
+-- Tạo filegroup
+ALTER DATABASE HOAYEUTHUONG ADD FILEGROUP FG1
+ALTER DATABASE HOAYEUTHUONG ADD FILEGROUP FG2
+ALTER DATABASE HOAYEUTHUONG ADD FILEGROUP FG3
+ALTER DATABASE HOAYEUTHUONG ADD FILEGROUP FG4
+
+-- Thêm data file vào mỗi filegroup
+ALTER DATABASE HOAYEUTHUONG ADD FILE (NAME = N'F1', FILENAME = N'D:\DATA\PartDB_F1.ndf') TO FILEGROUP FG1
+ALTER DATABASE HOAYEUTHUONG ADD FILE (NAME = N'F2', FILENAME = N'D:\DATA\PartDB_F2.ndf') TO FILEGROUP FG2
+ALTER DATABASE HOAYEUTHUONG ADD FILE (NAME = N'F3', FILENAME = N'D:\DATA\PartDB_F3.ndf') TO FILEGROUP FG3
+ALTER DATABASE HOAYEUTHUONG ADD FILE (NAME = N'F4', FILENAME = N'D:\DATA\PartDB_F4.ndf') TO FILEGROUP FG4
+
+-- Phân đoạn bảng
+
+
+-- Thực hiện lại
+DECLARE @Quy INT = 1
+DECLARE @Nam INT = 2021
+
+SELECT CH.TenCH, SUM(TongTien) AS 'Doanh Thu'
+FROM HOADON HD 
+JOIN HOADON_VANCHUYEN HD_VC ON HD.MaHD = HD_VC.MaHD
+JOIN CUAHANG CH ON HD.MaCH = CH.MaCH
+WHERE DATEPART(qq, HD_VC.ThoiGianGiao) = @Quy AND
+	  DATEPART(yy, HD_VC.ThoiGianGiao) = @Nam
+GROUP BY CH.MaCH, CH.TenCH
+
+------------------------- TỐI ƯU HÓA CÂU TRUY VẤN 6 -------------------------
+
+-- Tạo chỉ mục trên thuộc tính NGAYCHAM của bảng PHIEUCHAMCONG
+CREATE NONCLUSTERED INDEX IDX_TV6 ON PHIEUCHAMCONG(MANV)
+
+-- Thực hiện lại
+DECLARE @Thang INT = 1
+DECLARE @Nam_1 INT = 2021
+
+SELECT NV.MaNV, NV.TenNV, COUNT(PCC.NGAYCHAM) AS 'SONGAY'
+FROM NHANVIEN NV JOIN PHIEUCHAMCONG PCC ON PCC.MaNV = NV.MaNV
+WHERE DATEPART(MM, PCC.NGAYCHAM) = @Thang AND
+	  DATEPART(YY, PCC.NGAYCHAM) = @Nam_1
+GROUP BY NV.MaNV, NV.TenNV
+HAVING COUNT(PCC.GioCham) >= 2
+
---
+++ Query/TruyVan.sql
@@ -0,0 +1,58 @@
+﻿USE HOAYEUTHUONG
+GO 
+
+--Truy vấn 1: Tổng chi phí đặt hàng của tất cả khách hàng gửi, 
+--			  kết quả sắp xếp theo thứ tự giảm dần của tổng số tiền đã chi
+SELECT MaKHGui, SUM(TONGTIEN) AS 'Tổng đã chi'
+FROM HOADON HD 
+GROUP BY MaKHGui
+ORDER BY SUM(TONGTIEN) DESC
+
+--Truy vấn 2: Tìm kiếm sản phẩm theo chủ đề
+DECLARE @ChuDe VARCHAR(50)= 'Cây thông'
+
+SELECT MaSP, TenSP
+FROM SANPHAM_CHUDE
+WHERE ChuDe LIKE @ChuDe 
+
+--Truy vấn 3: Thống kê top n SP bán chạy nhất
+DECLARE @N INT = 100
+
+SELECT TOP(@N) SP.MaSP, SP.TenSP, SUM(CTHD.ThanhTien) AS 'Doanh số'
+FROM CT_HOADON CTHD JOIN SANPHAM SP ON CTHD.MaSP = SP.MaSP
+GROUP BY SP.MaSP, SP.TenSP
+ORDER BY SUM(CTHD.ThanhTien)
+
+--Truy vấn 4: Cửa hàng kiểm tra và gửi danh sách các nguyên liệu cần mua thêm
+DECLARE @MaCH INT = 18
+
+SELECT NL.MaNL, NL.TenNL, NL.SoLuongToiThieu - SL.SoLuongCH AS 'Số lượng cần mua thêm ít nhất'
+FROM NGUYENLIEU NL 
+JOIN SOLUONG_NL SL ON NL.MaNL = SL.MaNL 
+JOIN CUAHANG CH ON CH.MaCH = SL.MaCH
+--WHERE NL.SoLuongKHO < NL.SoLuongToiThieu
+WHERE CH.MaCH = @MaCH AND
+	  SL.SoLuongCH < NL.SoLuongToiThieu
+
+--Truy vấn 5: Thống kê doanh thu của từng cửa hàng theo quý (mỗi 3 tháng)
+DECLARE @Quy INT = 1
+DECLARE @Nam INT = 2021
+
+SELECT CH.TenCH, SUM(TongTien) AS 'Doanh Thu'
+FROM HOADON HD 
+JOIN HOADON_VANCHUYEN HD_VC ON HD.MaHD = HD_VC.MaHD
+JOIN CUAHANG CH ON HD.MaCH = CH.MaCH
+WHERE DATEPART(qq, HD_VC.ThoiGianGiao) = @Quy AND
+	  DATEPART(yy, HD_VC.ThoiGianGiao) = @Nam
+GROUP BY CH.MaCH, CH.TenCH
+
+--Truy vấn 6: Thống kê số ngày làm việc đủ 2 ca của từng nhân viên (theo tháng)
+DECLARE @Thang INT = 1
+DECLARE @Nam_1 INT = 2021
+
+SELECT NV.MaNV, NV.TenNV, COUNT(PCC.NGAYCHAM) AS 'SONGAY'
+FROM NHANVIEN NV JOIN PHIEUCHAMCONG PCC ON PCC.MaNV = NV.MaNV
+WHERE DATEPART(MM, PCC.NGAYCHAM) = @Thang AND
+	  DATEPART(YY, PCC.NGAYCHAM) = @Nam_1
+GROUP BY NV.MaNV, NV.TenNV
+HAVING COUNT(PCC.GioCham) >= 2
